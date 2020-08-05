9d183b23d8cb40a143d5638cf2f0665cfec96675
DanielSc4 <scalena99@gmail.com>
2020-08-05 12:13:46 +0200

    Updated main

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ import compute
 def main():
 	dataframes = init.importa()
 
-	compute.draw_casi(dataframes, frequency = "d")
-	compute.draw_by_region(dataframes, regione = "Lombardia", frequency = "d")
-	compute.draw_all_region(dataframes)
+	while (True):
+		print("1) Casi in Italia;\n2) Casi in una regione;\n3) Casi in tutte le regioni.\n4) Exit")
+		ino = input()
+		if (ino == "1"):
+			compute.draw_casi(dataframes, frequency = "d")
+		elif (ino == "2"):
+			regione = input("Inserisci la regione: ")
+			compute.draw_by_region(dataframes, regione, frequency = "d")
+		elif (ino == "3"):
+			compute.draw_all_region(dataframes)
+		elif (ino == "4"):
+			exit()
+		else:
+			pass
 
 
 if __name__ == "__main__":
